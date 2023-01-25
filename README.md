aaa91f6762ff9cb379df8a51a7a76bab450f2f41
jlshingler <jlshingler@users.noreply.github.com>
2023-01-24 19:53:53 -0500

    Updates
    Allow both left to right and right to left diagonal weave. Allow adjusting number of columns/warp ends (even number increments). Styling updates (preliminary).

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # WeavingDesignWebpage
- Simple webpage for planning out color designs for vertical and diagonal weaving patterns
+ Simple webpage for planning out color designs for vertical and diagonal warp-faced weaving patterns (specifically for Amtgard Fingerwoven Belts, but can be applied to other weaving styles, especially the vertical weave).
 
  Initial commit allows for basic color layouts for both vertical and diagonal weave structures and switching between Over/Under and Under/Over.
 
- Current limitations are the design only goes Left to Right and has a set number of warp ends (24), and colors are changed via toggle (rather than a picker). Additional functionality will be added later.
+ New additions Jan 24 2022: Diagonal Weave for both Left to Right and Right to Left. Allow for a range of warp ends/columns. Also tweaked some styling.
+
+ Current limitations are the colors are changed via toggle (rather than a picker), and there is still a limited number of weave/design types. Additional functionality will be added later.
--- app.js
+++ app.js
@@ -2,21 +2,49 @@
 
 /* Declare Global Variables */
 
+const decWarpBtn = document.querySelector('#dec-btn');
+const incWarpBtn = document.querySelector('#inc-btn');
 const changeWeaveBtn = document.querySelector('#weave-btn');
 const changeOverUnderBtn = document.querySelector('#over-under-btn');
 const reset = document.querySelector('.reset');
 const baseRow = document.querySelector('.base');
 const grid = document.querySelector('.grid');
-//const colorSelector = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'];
-const colorSelector = ['white', 'red', 'yellow', 'blue'];
-const defaultSize = 24;
-var columnCount = 24; // this one can be updated
+const colorSelector = ['white', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black'];
+//const colorSelector = ['white', 'red', 'yellow', 'blue'];
+const defaultSize = 14;
+var columnCount = 14; // this one can be updated
 var rowCount = 12;
 var isOverUnder = true;
-var weaveStyles = ['Vertical', 'Diagonal'];
+var weaveStyles = ['Vertical', 'Diagonal Left to Right', 'Diagonal Right to Left'];
 var weaveStyle = 'Vertical';
 
 /* Event Listeners */
+decWarpBtn.addEventListener('click', function () {
+    var elem = document.getElementById('warp');
+    columnCount = columnCount - 2;
+    if (columnCount < 6) {
+        columnCount = 6;
+    }
+    elem.textContent = columnCount;
+    resetCells();
+    clearGrids();
+    baseGrid(columnCount);
+    makeGrid(rowCount, columnCount);
+});
+
+incWarpBtn.addEventListener('click', function () {
+    var elem = document.getElementById('warp');
+    columnCount = columnCount + 2;
+    if (columnCount > 24) {
+        columnCount = 24;
+    }
+    elem.textContent = columnCount;
+    resetCells();
+    clearGrids();
+    baseGrid(columnCount);
+    makeGrid(rowCount, columnCount);
+});
+
 changeWeaveBtn.addEventListener('click', function () {
     var elem = document.getElementById('weave');
     let index = weaveStyles.indexOf(elem.textContent);
@@ -26,7 +54,7 @@ changeWeaveBtn.addEventListener('click', function () {
     }
     weaveStyle = weaveStyles[newIndex];
     elem.textContent = weaveStyle;
-    resetGrids();
+    resetCells();
 });
 
 changeOverUnderBtn.addEventListener('click', function () {
@@ -41,15 +69,15 @@ changeOverUnderBtn.addEventListener('click', function () {
     }
     elem.textContent = text;
     btn.textContent = btnText;
-    resetGrids();
+    resetCells();
 });
 
 reset.addEventListener('click', function () {
-    resetGrids();
+    resetCells();
 });
 
 /* Functions */
-function resetGrids() {
+function resetCells() {
     var baseElems = document.getElementsByClassName('base-item');
     var gridElems = document.getElementsByClassName('grid-item');
     for (let i = 0; i < baseElems.length; i++) {
@@ -60,16 +88,9 @@ function resetGrids() {
     }
 }
 
-function makeGrid(rows, cols) {
-    grid.style.setProperty('grid-template-columns', 'repeat(' + cols / 2 + ', 1fr)');
-    for (let i = 0; i < (rows); i++) {
-        for (let j = 0; j < (cols / 2); j++) {
-            let cell = document.createElement("div");
-            //cell.innerText = i;
-            cell.id = "grid-item-" + i + "-" + j;
-            grid.appendChild(cell).className = "grid-item";
-        }
-    };
+function clearGrids() {
+    baseRow.innerHTML = "";
+    grid.innerHTML = "";
 }
 
 function baseGrid(cols) {
@@ -82,6 +103,19 @@ function baseGrid(cols) {
         cell.onclick = function () { changeColor(cell); };
         baseRow.appendChild(cell);
     }
+}
+
+function makeGrid(rows, cols) {
+    grid.style.setProperty('grid-template-columns', 'repeat(' + cols / 2 + ', 1fr)');
+    for (let i = 0; i < (rows); i++) {
+        for (let j = 0; j < (cols / 2); j++) {
+            let cell = document.createElement("div");
+            //cell.innerText = i;
+            cell.id = "grid-item-" + i + "-" + j;
+            cell.style.setProperty('background-color', colorSelector[0]);
+            grid.appendChild(cell).className = "grid-item";
+        }
+    };
 }
 
 function changeColor(elem) {
@@ -129,14 +163,14 @@ function changeColor(elem) {
             }
         }
     }
-    if (weaveStyle == 'Diagonal') {
+    if (weaveStyle == 'Diagonal Right to Left') {
         for (let i = 0; i < rowCount; i++) {
             if (isOverUnder) {
                 // elemID = baseNum - row
                 // if elemID < 0: elemID = elemID + columnCount (reset to the end of the row)
                 // color based on which is odd vs even (ie which color is on top)
                 let elemID = baseNum - (i * 2);
-                if (elemID < 0) {
+                while (elemID < 0) { //for lower column counts, add in increments until we loop over
                     elemID = elemID + columnCount;
                 }
                 if (elemID < columnCount) {
@@ -157,7 +191,7 @@ function changeColor(elem) {
                 // if elemID < 0: elemID = elemID + columnCount (reset to the end of the row)
                 // color based on which is odd vs even (ie which color is on top)
                 let elemID = baseNum - (i * 2);
-                if (elemID < 0) {
+                while (elemID < 0) { //for lower column counts, add in increments until we loop over
                     elemID = elemID + columnCount;
                 }
                 if (elemID < rowCount * columnCount) {
@@ -176,10 +210,56 @@ function changeColor(elem) {
             }
         }
     }
-
+    if (weaveStyle == 'Diagonal Left to Right') {
+        for (let i = 0; i < rowCount; i++) {
+            if (isOverUnder) {
+                // elemID = baseNum - row
+                // if elemID >= columnCount: elemID = elemID - columnCount (reset to the beginning of the row)
+                // color based on which is odd vs even (ie which color is on top)
+                let elemID = baseNum + (i * 2);
+                while (elemID >= columnCount) { //for lower column counts, sub in increments until we loop over
+                    elemID = elemID - columnCount;
+                }
+                if (elemID < columnCount) {
+                    if (baseNum % 2 == 0) { // handle even base numbers
+                        if (i % 2 == 0) { // fill on even rows
+                            var elem = document.getElementById('grid-item-' + i + '-' + elemID / 2);
+                            elem.style.backgroundColor = colorSelector[newIndex];
+                        }
+                    } else { // handle odd base numbers
+                        if (i % 2 != 0) { // fill on odd rows
+                            var elem = document.getElementById('grid-item-' + i + '-' + (elemID - 1) / 2);
+                            elem.style.backgroundColor = colorSelector[newIndex];
+                        }
+                    }
+                }
+            } else { //Under Over
+                // elemID = baseNum - row
+                // if elemID >= columnCount: elemID = elemID - columnCount (reset to the beginning of the row)
+                // color based on which is odd vs even (ie which color is on top)
+                let elemID = baseNum + (i * 2);
+                while (elemID >= columnCount) { //for lower column counts, sub in increments until we loop over
+                    elemID = elemID - columnCount;
+                }
+                if (elemID < rowCount * columnCount) {
+                    if (baseNum % 2 == 0) { // handle even base numbers
+                        if (i % 2 != 0) { // fill on odd rows
+                            var elem = document.getElementById('grid-item-' + i + '-' + elemID / 2);
+                            elem.style.backgroundColor = colorSelector[newIndex];
+                        }
+                    } else { // handle odd base numbers
+                        if (i % 2 == 0) { // fill on even rows
+                            var elem = document.getElementById('grid-item-' + i + '-' + (elemID - 1) / 2);
+                            elem.style.backgroundColor = colorSelector[newIndex];
+                        }
+                    }
+                }
+            }
+        }
+    }
 };
 
 /* Call Functions */
 
 baseGrid(defaultSize); //default cols
-makeGrid(rowCount, defaultSize);
+makeGrid(rowCount, defaultSize); 
